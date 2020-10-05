2504687c3c647624f1eded0ddc329846e16636f8
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-10-05 21:45:26 +0200

    Generating first Entity

---
+++ migrations/Version20201005194029.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201005194029 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE blog_post ADD content LONGTEXT NOT NULL, ADD author VARCHAR(255) NOT NULL, CHANGE published published DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE blog_post DROP content, DROP author, CHANGE published published VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
