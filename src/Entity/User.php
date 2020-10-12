d855c12dd2aaf1b6412ada57a15c504e17e32cbd
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-10-12 13:35:28 +0200

    Built-in API Platform operations

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,8 +7,10 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
 class User implements UserInterface
