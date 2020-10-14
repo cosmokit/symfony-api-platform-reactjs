e355c9f42281746086ae33d9abc9d27cf2f2e251
G.Charkiewicz <charkiewicz.grzegorz@gmail.com>
2020-10-14 09:22:33 +0200

    Validating uniqueness of fields (username, email)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -20,6 +21,8 @@ use Symfony\Component\Validator\Constraints as Assert;
  *     }
  * )
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * unieqe entity (fields = {"email", "name"})
+ * @UniqueEntity("email")
  */
 class User implements UserInterface
 {
